63f6d2c10f6a9294b7b5f2564d2a92075844f321
razrab345 <razrab345@gmail.com>
2026-04-05 16:44:14 +0500

    update

--- frontend1/.output/public/_nuxt/QczXU5kt.js
+++
@@ -1,1 +0,0 @@
-import{av as a}from"./CiOG5EzB.js";var s=a();export{s as O};
--- frontend1/nuxt.config.ts
+++ frontend1/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: process.env.NUXT_APP_BASE_URL || '/'
+  },
   modules: [
     '@nuxt/eslint',
     '@primevue/nuxt-module',
